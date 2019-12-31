0520a55f96809646de52e39bf93a5e934874463a
Doak Covington <doakcovington@protonmail.com>
2019-12-31 03:28:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
   elsif array.length >= 1
     sentence = ""
     array.each do |spot,index|
-      sentence = "#{spot[index]} #{spot}"
+      sentence = "#{spot[index]}. #{spot}"
     end
     puts sentence
   end
