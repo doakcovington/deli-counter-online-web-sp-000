a1fa6defd459b293a0c6489b3090b38d866f3f35
Doak Covington <doakcovington@protonmail.com>
2019-12-31 03:21:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   elsif array.length >= 1
-    converted_sentence = array.join()
-      puts converted_sentence
-    end
+    sentence = array.join()
+    puts sentence
+    
   end
 end
