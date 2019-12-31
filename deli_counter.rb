011cf410aaa47411fbbaad46cfebca42f2839386
Doak Covington <doakcovington@protonmail.com>
2019-12-31 03:28:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(array)
     puts "The line is currently empty."
   elsif array.length >= 1
     sentence = ""
-    array.each do |spot|
-      sentence = "#{spot[]} #{spot}"
+    array.each do |spot,index|
+      sentence = "#{spot[index]} #{spot}"
     end
     puts sentence
   end
