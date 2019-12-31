2ae79c063a3999754490de21aaec805fd6697662
Doak Covington <doakcovington@protonmail.com>
2019-12-31 03:17:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,8 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   elsif array.length >= 1
-    puts array.join()
+    array.each do |number|
+      array.join("#{[number]} #{number}")
+    end
   end
 end
